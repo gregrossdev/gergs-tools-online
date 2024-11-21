93618cded13a4085e2c5cc59c6534475cb415be8
Greg Ross <gregross.dev@gmail.com>
2024-11-21 18:14:13 -0500

    spring starter

---
+++ src/main/java/dev/gregross/gergstoolsonline/GergsToolsOnlineApplication.java
@@ -0,0 +1,13 @@
+package dev.gregross.gergstoolsonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GergsToolsOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GergsToolsOnlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/gregross/gergstoolsonline/GergsToolsOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gregross.gergstoolsonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GergsToolsOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
