321d2468de615ed52b9ce5e3d808b032c98ee68d
Greg Ross <gregross.dev@gmail.com>
2024-11-21 22:07:40 -0500

    controller find tool by id and not found exception

--- src/main/java/dev/gregross/gergstoolsonline/system/exception/ExceptionHandlerAdvice.java
+++ src/main/java/dev/gregross/gergstoolsonline/system/exception/ExceptionHandlerAdvice.java
@@ -1,10 +1,21 @@
 package dev.gregross.gergstoolsonline.system.exception;
 
+import dev.gregross.gergstoolsonline.system.Result;
+import dev.gregross.gergstoolsonline.system.StatusCode;
+import dev.gregross.gergstoolsonline.tool.ToolNotFoundException;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ExceptionHandlerAdvice {
+
+	@ExceptionHandler(ToolNotFoundException.class)
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	Result handleArtifactNotFoundException(ToolNotFoundException ex){
+		return new Result(false, StatusCode.NOT_FOUND, ex.getMessage());
+	}
 
 
 }
--- src/main/java/dev/gregross/gergstoolsonline/tool/ToolController.java
+++ src/main/java/dev/gregross/gergstoolsonline/tool/ToolController.java
@@ -1,6 +1,7 @@
 package dev.gregross.gergstoolsonline.tool;
 
 import dev.gregross.gergstoolsonline.system.Result;
+import dev.gregross.gergstoolsonline.system.StatusCode;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,11 +11,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/tools")
 public class ToolController {
 
+	private final ToolService toolService;
 
-	@GetMapping("/{toolId}")
-	public Result findArtifactById(@PathVariable String toolId) {
-		return null;
+	public ToolController(ToolService toolService) {
+		this.toolService = toolService;
 	}
 
-	
+	@GetMapping("/{toolId}")
+	public Result findToolById(@PathVariable String toolId) {
+		Tool foundTool = toolService.findById(toolId);
+		return new Result(true, StatusCode.SUCCESS, "Find One Success", foundTool);
+	}
+
+
 }
